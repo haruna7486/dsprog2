87f98c3e1559b98bcdde3c21cf5da2e4abaf7d31
haruna7486 <166205141+haruna7486@users.noreply.github.com>
2024-12-03 14:00:35 +0900

    定数を定義

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ import requests
 import flet as ft
 import json
 from datetime import datetime
+
+# 定数を定義
+DATA_FILE = "jma/weather_info.json"
+WEATHER_API_URL = "https://www.jma.go.jp/bosai/forecast/data/forecast/{office_code}.json"
